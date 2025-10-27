d2c5577865655fe7962f2b21d15c969cf45123af
dishitha1205 <dishitha1205@gmail.com>
2025-10-27 15:01:56 +0530

    Add placeholder files for model and java-app (so folders appear on GitHub)

---
+++ model/food_calorie_classifier.py
@@ -0,0 +1,10 @@
+# starter file: simple placeholder for your model
+"""
+food_calorie_classifier.py
+Starter code (place holder). Replace with your training script later.
+"""
+def hello():
+    print("Food Calorie Classifier placeholder")
+
+if __name__ == "__main__":
+    hello()
